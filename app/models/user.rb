32656d701c9d954f812af6acf94f6710575af46e
Masutani Kie <hayulum47@gmail.com>
2020-03-12 17:01:45 +0900

    [change]  ログイン修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["name = :value OR lower(email) = lower(:value", { :value => login}]).first
+      where(conditions).where(["name = :value OR lower(email) = lower(:value)", { :value => login }]).first
     else
       where(conditions).first
     end
