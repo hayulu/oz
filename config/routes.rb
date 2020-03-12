08c084e2feb006f669648f46ea7999282eb18a38
Masutani Kie <hayulum47@gmail.com>
2020-03-12 14:04:28 +0900

    [add]userモデルの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   get 'static_pages/index'
   root 'static_pages#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = %w[yamada abe tanaka yave kitani]
+users.each_with_index do |u, i|
+  User.create(
+    name: u.to_s,
+    email: "#{i + 1}@gamil.com",
+    password: 'password',
+    address: '大阪府大阪市北区'
+  )
+end
